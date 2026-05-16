91412d8f3a612ffa3a96af7365d3d80f444572b4
RattlingJester <slawa.yakim@yandex.ru>
2026-05-16 17:58:49 +0300

    Added re exports from nalgebra

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ pub mod kinematics;
 pub mod node;
 pub mod trajectory;
 
+pub use nalgebra::{Isometry3, SMatrix, Translation3, Unit, UnitQuaternion, Vector3};
+
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(Debug)]
 pub enum Error {
