4da77a8e80e10fab62376e181ad8ffbdc8c3e7f2
RattlingJester <slawa.yakim@yandex.ru>
2026-06-02 19:06:11 +0300

    Update ik.rs

--- src/ik.rs
+++ src/ik.rs
@@ -1,7 +1,74 @@
-use nalgebra::{Isometry3, RealField};
+#![allow(unused)]
+
+use nalgebra::{Isometry3, RealField, SVector};
 use simba::scalar::SubsetOf;
 
-use crate::{kinematics::Chain, node::NodeIDx};
+use crate::{Error, kinematics::Chain, node::NodeIDx};
+
+#[derive(Debug)]
+pub struct JacobianIK<const JOINTS: usize, T: RealField, F: Fn(&[T]) -> [T; JOINTS] + Send + Sync> {
+	pub allowable_error_dist:  T,
+	pub allowable_error_angle: T,
+	pub jacobian_mult:         T,
+	pub max_try:               usize,
+	pub nullpace_fn:           Option<F>,
+}
+
+impl<const J: usize, T: RealField + SubsetOf<f64>, F: Fn(&[T]) -> [T; J] + Send + Sync> Default
+	for JacobianIK<J, T, F>
+{
+	fn default() -> Self {
+		Self::new(
+			nalgebra::convert(0.001),
+			nalgebra::convert(0.1),
+			nalgebra::convert(1.0),
+			1000,
+		)
+	}
+}
+
+impl<const JOINTS: usize, T: RealField + SubsetOf<f64>, F: Fn(&[T]) -> [T; JOINTS] + Send + Sync>
+	JacobianIK<JOINTS, T, F>
+{
+	pub fn new(
+		allowable_error_dist: T,
+		allowable_error_angle: T,
+		jacobian_mult: T,
+		max_tries: usize,
+	) -> Self {
+		Self {
+			allowable_error_dist,
+			allowable_error_angle,
+			jacobian_mult,
+			max_try: max_tries,
+			nullpace_fn: None,
+		}
+	}
+
+	fn solve<const DOF: usize>(
+		&mut self,
+		chain: &Chain<DOF, JOINTS, T>,
+		constraints: Constraints<JOINTS>,
+	) -> Result<SVector<T, DOF>, Error> {
+		let op_space = constraints.operational_space();
+		let orig_positions = chain.joint_positions();
+
+		let required_dof = op_space.iter().filter(|x| **x).count();
+		let available_dof = DOF - constraints.inored_joints.len();
+		if available_dof < required_dof {
+			return Err(Error::SizeMismatch {
+				provided: available_dof,
+				expected: required_dof,
+			});
+		}
+
+		let mut last_target_distance = None;
+
+		for _ in 0..self.max_try {}
+
+		todo!()
+	}
+}
 
 #[derive(Debug)]
 pub struct Constraints<const J: usize> {
@@ -14,64 +81,15 @@ pub struct Constraints<const J: usize> {
 	pub inored_joints: [NodeIDx; J],
 }
 
-#[derive(Debug)]
-pub struct JacobianIK<const J: usize, T: RealField, F: Fn(&[T]) -> [T; J] + Send + Sync> {
-	pub allowable_error_dist:  T,
-	pub allowable_error_angle: T,
-	pub jacobian_mult:         T,
-	pub max_tries:             usize,
-	pub nullpace_fn:           Option<F>,
-}
-
-impl<const J: usize, T: RealField, F: Fn(&[T]) -> [T; J] + Send + Sync> Default
-	for JacobianIK<J, T, F>
-{
-	fn default() -> Self {
-		Self {
-			allowable_error_dist:  nalgebra::convert(0.001),
-			allowable_error_angle: nalgebra::convert(0.1),
-			jacobian_mult:         nalgebra::convert(1.0),
-			max_tries:             1000,
-			nullpace_fn:           None,
-		}
-	}
-}
-
-impl<const J: usize, T: RealField + SubsetOf<f64>, F: Fn(&[T]) -> [T; J] + Send + Sync>
-	JacobianIK<J, T, F>
-{
-	pub fn new(
-		allowable_error_dist: T,
-		allowable_error_angle: T,
-		jacobian_mult: T,
-		max_tries: usize,
-	) -> Self {
-		Self {
-			allowable_error_dist,
-			allowable_error_angle,
-			jacobian_mult,
-			max_tries,
-			nullpace_fn: None,
-		}
-	}
-
-	pub fn solve<const DOF: usize>(
-		&self,
-		arm: Chain<DOF, J, T>,
-		target: Isometry3<T>,
-		constraints: &Constraints<J>,
-	) -> Result<(), crate::Error> {
-		let orig_pos = arm.joints_positions();
-
-		todo!()
-	}
-
-	fn solve_with_constraints<const DOF: usize>(
-		&self,
-		arm: Chain<DOF, J, T>,
-		target: Isometry3<T>,
-		constraints: &Constraints<J>,
-	) -> Result<(), crate::Error> {
-		todo!()
+impl<const J: usize> Constraints<J> {
+	fn operational_space(&self) -> [bool; 6] {
+		let mut arr = [true; 6];
+		arr[0] = self.position_x;
+		arr[1] = self.position_y;
+		arr[2] = self.position_z;
+		arr[3] = self.rotation_x;
+		arr[4] = self.rotation_y;
+		arr[5] = self.rotation_z;
+		arr
 	}
 }
